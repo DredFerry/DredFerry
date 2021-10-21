7847cbf6564b6e5ec3a616de5aaa2103dab8d817
Dred <92103630+DredFerry@users.noreply.github.com>
2021-10-21 22:36:05 +0530

    Add files via upload

---
+++ Project4/pg1.js
@@ -0,0 +1,11 @@
+function opensearch()
+{
+    document.getElementById('searchtab').style.height="20vh"
+    document.getElementById('searchtab').style.transition="ease-out .5s "
+
+}
+function closeSearch()
+{
+    document.getElementById('searchtab').style.height="0vh"
+    document.getElementById('searchtab').style.transition="ease-out .5s "
+}
